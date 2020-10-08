06857dbc0aa9e3d54a2c8824827ecaec5b373572
argorudip <argo.rudi@gmail.com>
2020-10-08 13:34:20 +0700

    v11

---
+++ app/src/main/java/com/argorudip/pengumuman/MainActivity.java
@@ -0,0 +1,15 @@
+package com.argorudip.pengumuman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
